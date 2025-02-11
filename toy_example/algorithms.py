9458d364301a5646c58421bdd0c29e544dc3babb
Paula Cordero Encinar <pc2222@ic.ac.uk>
2025-02-11 10:05:57 +0000

    simulation study toy example

--- toy_example/algorithms.py
+++ toy_example/algorithms.py
@@ -1,6 +1,9 @@
 import os
 import sys
 
+#######################
+## TOY EXAMPLE
+#######################
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(project_root)
